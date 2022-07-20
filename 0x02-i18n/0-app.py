5626b1af2f4f3eee82a269d19ec6e2367972b031
evancechapuma62@gmail.com <evancechapuma62@gmail.com>
2022-07-20 08:10:50 +0200

    pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,1 +1,10 @@
-#!/usr/bin/env python3 ''' flask application ''' from flask import Flask, render_template app = Flask(__name__) @app.route('/') def index(): '''0-index.html.''' return render_template("0-index.html") if __name__ == "__main__": app.run(host="0.0.0.0", port="5000")
+#!/usr/bin/env python3
+'''flask application '''
+from flask import Flask, render_template
+app = Flask(__name__) @app.route('/')
+def index():
+  '''0-index.html.'''
+  return render_template("0-index.html")
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port="5000")
