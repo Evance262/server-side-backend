5259e22c9d39fb474210b9966a83f8ccaf8707a5
Evance262 <evancechapuma62@gmail.com>
2022-07-22 15:29:35 +0200

    Hello Holberton

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,12 +2,14 @@
 '''flask application '''
 from flask import Flask, render_template
 
-app = Flask(__name__) @app.route('/')
+app = Flask(__name__)
 
 
+@app.route('/')
 def index():
-  '''0-index.html.'''
-  return render_template("0-index.html")
+    '''0-index.html.'''
+    return render_template("0-index.html")
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000")
