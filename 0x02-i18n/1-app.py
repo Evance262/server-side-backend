7719bc8a453be09e1d8b2a0af38b7706527b0bde
Evance262 <evancechapuma62@gmail.com>
2022-07-22 15:55:32 +0200

    setting a locale

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''Babel application'''
+from flask_babel import Babel
+from flask import request
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    '''1-index.html'''
+    return render_template("1-index.html")
+
+class Config(object):
+    '''instantiating the babel object'''
+    LANGUAGES = ["en", "fr"]
+    TIMEZONE = ["UTC"]
+
+
+@babel.localeselector
+def get_locale():
+    return locale.setlocale(LANGUAGES, TIMEZONE)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
