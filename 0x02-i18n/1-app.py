39350d1ae2d9d2baca36600f61d4cd4cf21c7e74
Evance262 <evancechapuma62@gmail.com>
2022-07-22 16:43:05 +0200

    setting a locale

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 '''Babel application'''
 from flask_babel import Babel
-from flask import g, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 babel = Babel(app)
 
+
+class Config(object):
+    '''instantiating the babel object'''
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.from_object(Config)
+Babel.default_locale = "en"
+Babel.default_timezone = "UTC"
 
 @app.route('/')
 def index():
@@ -14,25 +22,5 @@ def index():
     return render_template("1-index.html")
 
 
-class Config(object):
-    '''instantiating the babel object'''
-    LANGUAGES = ["en", "fr"]
-    TIMEZONE = ["UTC"]
-
-
-@babel.localeselector
-def get_locale():
-    '''selecting a language tlanslation to use'''
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@babel.timezoneselector
-def get_timezone():
-    '''defaulting the timezone to UTC'''
-    user = getattr(g, 'user', None)
-    if user is not None:
-        return user.timezone.best_match(app.config['TIMEZONE'])
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run()
